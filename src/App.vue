9b76b189b6717772f200d236fc20d00746365266
Chemex <chemex@MacBook-Pro-de-Chemex.local>
2025-05-13 17:25:31 -0600

    ui improvements

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { Notivue, Notification, NotificationProgress, push } from 'notivue'
 </script>
 
 <template>
-  <button @click="push.success('Hello from your first notification!')">
-    Push
-  </button>
-
-  <Notivue v-slot="item">
-    <Notification :item="item">
-      <NotificationProgress />
-    </Notification>
-  </Notivue>
   <RouterView />
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import Unimport from 'unimport/unplugin'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    Unimport.vite({
+      addons: {
+        vueTemplate: true,
+      },
+      imports: [{ name: 'push', from: 'notivue' }],
+    }),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
